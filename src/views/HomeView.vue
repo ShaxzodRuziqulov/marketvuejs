b371eeb77909d9ab8ab2df52681d7c3d0e450f64
Shaxzod <janobprizident@gmail.com>
2025-04-16 17:04:53 +0500

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,4 @@
 <template>
   <h1 class="text-red-500 font-bold text-2xl">Maxsulotlar saxifasi</h1>
 </template>
+<script></script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-blue-500 font-bold text-2xl">Product saxifasi</h1>
+
 </template>
 <script>
 export default {
