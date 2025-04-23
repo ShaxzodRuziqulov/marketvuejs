d87e7a1e1fdeb3d791430f51d1764754fa927177
Shaxzod <janobprizident@gmail.com>
2025-04-23 19:50:02 +0500

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -50,7 +50,6 @@
 
 <script setup lang="ts">
 import {computed, onMounted, ref} from 'vue'
-import Chart from 'chart.js/auto'
 import router from "../router";
 import {DashboardService} from "../service/DashboardService.ts";
 
